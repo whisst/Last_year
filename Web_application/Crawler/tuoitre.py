9d9c488f7be1be0272f312f9df635ffea43aed26
Hoàng Nhật Tân <43608871+whisst@users.noreply.github.com>
2020-12-02 20:47:51 +0700

    Update tuoitre.py

--- Web_application/Crawler/tuoitre.py
+++ Web_application/Crawler/tuoitre.py
@@ -10,24 +10,40 @@ page = requests.get("https://tuoitre.vn/suc-khoe/phong-mach/trang-"+str(i)+".htm
 soup = bs4.BeautifulSoup(page.content,"html.parser")
 
 atribute = soup.find_all("li",{"class":"news-item"})
-for i in range(1,41):
+for j in atribute:
+    title = j.find('a')
+    img = j.find('img')
+    titles.append(title.get('title'))
+    imgLinks.append(img.get('src'))
+    imgCaps.append(img.get('alt'))
+
+print('\n'.join(titles))
+print('\n')
+print('\n'.join(imgCaps))
+print('\n')
+
+import requests
+import bs4
+
+titles = []
+imgCaps = []
+imgLinks = []
+
+
+for i in range(1,10):
     page = requests.get("https://tuoitre.vn/suc-khoe/phong-mach/trang-"+str(i)+".htm")
-    soup = BeautifulSoup(page.content,"html.parser")
+    soup = bs4.BeautifulSoup(page.content,"html.parser")
 
-    if (i == 1):
-        img = soup.find('img',{'class':'img324x202'})
-        crawl(img)
-        img = soup.find_all('img',{'class':'img124x77'})
-        for j in img:
-            crawl(j)
-    img = soup.find_all('img',{'class':'img212x132'})
-    for j in img:
-        crawl(j)
+    atribute = soup.find_all("li",{"class":"news-item"})
+    for j in atribute:
+        title = j.find('a')
+        img = j.find('img')
+        titles.append(title.get('title'))
+        imgLinks.append(img.get('src'))
+        imgCaps.append(img.get('alt'))
 
-data = {
-    "Image Caption": imgCaps,
-    "Image URL": imgLinks
-}
-df = pandas.DataFrame(data)
-df.to_csv(r'..\UI_demo_II\Tuoitre.csv', index = False)
-
+print('\n'.join(titles))
+print('\n')
+print('\n'.join(imgCaps))
+print('\n')
+print('\n'.join(imgLinks))
