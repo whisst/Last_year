31d61e1c1a04b2e4ff8330c1a093d4cf7f844212
Hoàng Nhật Tân <43608871+whisst@users.noreply.github.com>
2020-12-02 20:09:16 +0700

    Update tuoitre.py

--- Web_application/Crawler/tuoitre.py
+++ Web_application/Crawler/tuoitre.py
@@ -10,15 +10,24 @@ page = requests.get("https://tuoitre.vn/suc-khoe/phong-mach/trang-"+str(i)+".htm
 soup = bs4.BeautifulSoup(page.content,"html.parser")
 
 atribute = soup.find_all("li",{"class":"news-item"})
-for j in atribute:
-    title = j.find('a')
-    img = j.find('img')
-    titles.append(title.get('title'))
-    imgLinks.append(img.get('src'))
-    imgCaps.append(img.get('alt'))
+for i in range(1,41):
+    page = requests.get("https://tuoitre.vn/suc-khoe/phong-mach/trang-"+str(i)+".htm")
+    soup = BeautifulSoup(page.content,"html.parser")
 
-print('\n'.join(titles))
-print('\n')
-print('\n'.join(imgCaps))
-print('\n')
+    if (i == 1):
+        img = soup.find('img',{'class':'img324x202'})
+        crawl(img)
+        img = soup.find_all('img',{'class':'img124x77'})
+        for j in img:
+            crawl(j)
+    img = soup.find_all('img',{'class':'img212x132'})
+    for j in img:
+        crawl(j)
+
+data = {
+    "Image Caption": imgCaps,
+    "Image URL": imgLinks
+}
+df = pandas.DataFrame(data)
+df.to_csv(r'..\UI_demo_II\Tuoitre.csv', index = False)
 
